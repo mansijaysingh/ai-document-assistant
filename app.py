1835668a0cfc9f1e0d5cb1d92233c8ca5f6c4604
Mansijaysingh <mansisingh.am000@gmail.com>
2025-08-26 16:07:34 +0530

    file save

--- app.py
+++ app.py
@@ -10,3 +10,31 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
+load_dotenv()
+
+app=Flask(__name__)
+
+UPLOAD_FOLDER='uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+  os.mkdir(UPLOAD_FOLDER)
+
+
+llm=ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.7)
+
+vector_store=None
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/upload',methods=['POST'])
+def upload_file():
+  if 'pdf_file' in request.files:
+    file=request.files['pdf_file']
+    if file.filename !='':
+      filepath=os.path.join(UPLOAD_FOLDER, file.filename)
+      file.save(filepath)
+
+      message="File uploaded and processed successfully."
+      return render_template("index.html", message=message)
+    
